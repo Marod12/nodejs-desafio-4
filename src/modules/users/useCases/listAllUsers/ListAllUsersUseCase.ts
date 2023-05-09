791683f1eac078fe10cded52e1541b8ef453a9ad
Marod <mathews-rodrigues@hotmail.com>
2023-05-08 21:38:02 -0300

    correção do código

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExists.admin) {
+      throw new Error("User is not admin");
+    }
+
+    if (!userAlreadyExists) {
+      throw new Error("User not already exists");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
